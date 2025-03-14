d90c47aceec6021272b51fe091fe6032a44fb774
clankpan <clankpan26cm@gmail.com>
2025-03-14 14:51:51 +0900

    wip

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e  # stop script if error happens
 
-pandoc ./docs/README.md -o docs/index.html --mathjax  --toc --standalone
+pandoc README.md -o docs/index.html --mathjax  --toc --standalone
